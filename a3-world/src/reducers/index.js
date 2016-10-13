97d226218633126f27d541135b0db6d2b0e85d12
Ece Ozalp <github email address>
2016-10-12 22:52:10 -0400

    react-router added

---
+++ a3-world/src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+// import runsReducer from './runs_reducer'
+
+const rootReducer =  combineReducers({
+//   runs: runsReducer,
+})
+
+export default rootReducer;
