07062c95c490461115a68aac46565f6eb807e1a6
Ece Ozalp <github email address>
2016-11-07 10:53:09 -0500

    haiku added, about page styled

--- a3-world/src/App.js
+++ a3-world/src/App.js
@@ -11,27 +11,30 @@ class App extends Component {
   
   constructor () {
     super()
-    this.state = {lineOne: "", lineTwo: "", lineThree: ""}
+    this.state = {lineZero: "Haiku of the Week", lineOne: "", lineTwo: "", lineThree: ""}
     this.populatePoemLines = this.populatePoemLines.bind(this)
   }
 
   populatePoemLines() {
     setTimeout( function () {
       this.setState({lineOne: "my functions return"})
-    }.bind(this), 1000)
+    }.bind(this), 1500)
     setTimeout( function () {
       this.setState({lineTwo: "true, commits merge conflict free"})
-    }.bind(this), 2500)
+    }.bind(this), 3000)
     setTimeout( function () {
       this.setState({lineThree: "my terminal clear"})
-    }.bind(this), 4000)
+    }.bind(this), 4500)
   }
 
   emptyHomeMessage () {
     if (!this.props.children) {
       return (
             <h5>
-              <div className="line" key={1} >
+              <div className="line" style={{color:"rgb(204,0,0)", "text-align":"center", padding:"10px"}} key={0} >
+                  {this.state.lineZero}
+              </div>
+              <div className="line" key={1}>
                   {this.state.lineOne}
               </div>
               <div className="line" key={2}>
--- a3-world/src/components/about_me.js
+++ a3-world/src/components/about_me.js
@@ -1,26 +1,52 @@
 import React from 'react'
-import { Button, ButtonToolbar, Popover, OverlayTrigger, Transition } from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
 
 function About () {
 
     return (
-        <div>
+        <div className="first">
+           
                 <br/>
-            <h2> Ece Özalp</h2>
-                <br/>
+                    <h2> Ece Özalp</h2>
+
+
             <h3> Full Stack Web Developer</h3>
             <h4> Ruby on Rails, Javascript, React.js, Redux, ActiveRecord, </h4>
             <h4> jQuery, SQL, HTML / CSS, Heroku, Git, Node.js</h4> 
-                 <br/> 
+          
+        
+          
+          
+                  <div className="about-info">
+                    <div className="about-info-one">
+                        <a href="http://www.github.com/ecezalp" target="_blank">
+                            <Button className="black-button" bsStyle="warning" bsSize="medium">
+                                    GitHub
+                            </Button>
+                        </a>
+                    </div>
+
+                    <div className="about-info-two">
+                        <a href="https://www.linkedin.com/in/ece-ozalp-46b18940" target="_blank">
+                            <Button className="black-button" bsStyle="warning" bsSize="medium">
+                                    LinkedIn
+                            </Button>
+                        </a>
+                    </div>
+                </div>
+
+            <br />
+                       <br />
+                        <br />
             <h3> Web Dev Immersive (Rails & Javascript) </h3>
             <h4> Flatiron School 2016 </h4>
-                 <br/>
+       
             <h3> MFA Creative Writing </h3>
             <h4> The New School 2016 </h4>
-                 <br/>
-            <h3> BA Literature && BA Political Science && Minor Italian </h3>
+        
+            <h3> BA Literature & BA Political Science & Minor Italian </h3>
             <h4> Duke University 2012 </h4>
-                <br/>
+       
                 <br/>
                 <br/>
         </div>
