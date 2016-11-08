f3319c1eda945a3d1b6105580c3dc7500fc393e4
Ece Ozalp <github email address>
2016-11-07 21:52:48 -0500

    styling

--- a3-world/src/components/about_me.js
+++ a3-world/src/components/about_me.js
@@ -7,7 +7,6 @@ function About () {
         <div className="first">
                 <br/>
             <h2> Ece Özalp</h2>
-                <br/>
                 <div className="about-info">
                     <div className="about-info-one">
                         <a href="http://www.github.com/ecezalp" target="_blank">
@@ -25,19 +24,17 @@ function About () {
                     </div>
                 </div>
                     <br/>
-                    <br/>
-                    <br/>
-            <h3> Full Stack Web Developer</h3>
+            <h3 style={{"margin-top":"50px"}}> Full Stack Web Developer</h3>
             <h4> Ruby on Rails, Javascript, React.js, Redux, ActiveRecord, </h4>
             <h4> jQuery, SQL, HTML / CSS, Heroku, Git, Node.js</h4> 
-                <br/>     
-            <h3> Web Dev Immersive (Rails & Javascript) </h3>
+           
+            <h3 style={{"margin-top":"40px"}}> Web Dev Immersive (Rails & Javascript) </h3>
             <h4> Flatiron School 2016 </h4>
     
-            <h3> MFA Creative Writing </h3>
+            <h3 style={{"margin-top":"40px"}}> MFA Creative Writing </h3>
             <h4> The New School 2016 </h4>
         
-            <h3> BA Literature & BA Political Science & Minor Italian </h3>
+            <h3 style={{"margin-top":"40px"}}> BA Literature & BA Political Science & Minor Italian </h3>
             <h4> Duke University 2012 </h4>
                 <br/>
                 <br/>
